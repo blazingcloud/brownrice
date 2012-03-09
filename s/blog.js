836c1d42cbf349e6e8e6a6e4e3893e3d2e34606b
Curtis Schofield <curtis@ram9.cc>
2012-03-09 15:24:15 -0800

    experiment complete - move to server side

--- s/blog.js
+++ s/blog.js
@@ -1,11 +1,14 @@
 console.log('blog.js')
 
 var TEMPLATE_URI= 'blog.mustache?callback=?'
-var template = $.get(TEMPLATE_URI)
+$.get(TEMPLATE_URI,function(_template) {
+  var template = _template
 var process_blog = function(data) {
-console.log(data)
+  for(var i = 0, length = data.length; i < length; i++) {
+    $('.main').append(Mustache.render(template, data[i]))
+  }
 }
-
 $.getJSON('http://blazingcloud.net/feed/json?callback=?',process_blog)
+})
 
 
